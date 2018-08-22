c258f6338d769e280e49567478b4f58e303228b8
许锦伟 <xujinwei@xujinweideimac.local>
2018-08-22 17:57:51 +0800

    Initial Commit

---
+++ jumpingView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  jumpingView
+//
+//  Created by 许锦伟 on 2018/8/22.
+//  Copyright © 2018年 许锦伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ jumpingView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  jumpingView
+//
+//  Created by 许锦伟 on 2018/8/22.
+//  Copyright © 2018年 许锦伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
